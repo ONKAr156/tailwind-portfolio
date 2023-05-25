93a5a77c0b150cab2c5ad2b5b9364bc043701b36
ONKAR <borgaonkar1998@gmail.com>
2023-05-25 11:46:12 +0530

    updated portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -12,10 +14,16 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
-}) {
+}) 
+
+{
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar/>
+        {children}
+        <Footer/>        
+        </body>
     </html>
   )
 }
